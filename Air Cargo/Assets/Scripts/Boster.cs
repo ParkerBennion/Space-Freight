7be7c186afe3c7d1d5adfd166470aed74e2ca874
Parker <parkerbennion@gmail.com>
2020-06-01 22:29:13 -0600

    first comit

---
+++ Air Cargo/Assets/Scripts/Boster.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Boster : MonoBehaviour
+{
+    public int speed =20;
+    private Rigidbody Plane;
+
+    private void Awake()
+    {
+        Plane = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+           Plane.AddForce( Vector3.back*speed*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Plane.AddForce(Vector3.forward*speed*Time.deltaTime);
+        }
+    }
+}
---
+++ Air Cargo/Assets/Scripts/LeftBoost.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LeftBoost : MonoBehaviour
+{
+    public int speed =20;
+    private Rigidbody Plane;
+
+    private void Awake()
+    {
+        Plane = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Plane.AddForce( Vector3.back*speed*Time.deltaTime);
+        }
+    }
+}
